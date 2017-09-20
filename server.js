65c02a3bc083b32b9f09ad3a7bcf6170bcbb87e6
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2017-09-20 11:32:09 -0300

    Change keys

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.set('port', port);
 
 
 
-
-
 var fs = require('fs')
     , ursa = require('ursa')
     , crt
@@ -44,31 +42,6 @@ app.post('/decrypt', function (req, res) {
         res.status(400).json({ error: true, description: "Bad request, make sure you are sending the right parameters." });
     }
 });
-
-
-
-// console.log('Encrypt with Public');
-// msg = crt.encrypt("Everything is going to be 200 OK", 'utf8', 'base64');
-// console.log('encrypted', msg, '\n');
-
-// console.log('Decrypt with Private');
-// msg = key.decrypt(msg, 'base64', 'utf8');
-// console.log('decrypted', msg, '\n');
-
-// console.log('############################################');
-// console.log('Reverse Public -> Private, Private -> Public');
-// console.log('############################################\n');
-
-// console.log('Encrypt with Private (called public)');
-// msg = key.privateEncrypt("Everything is going to be 200 OK", 'utf8', 'base64');
-// console.log('encrypted', msg, '\n');
-
-// console.log('Decrypt with Public (called private)');
-// msg = crt.publicDecrypt(msg, 'base64', 'utf8');
-// console.log('decrypted', msg, '\n');
-
-
-
 
 // Listen on the specified port
 app.listen(port, function () {
